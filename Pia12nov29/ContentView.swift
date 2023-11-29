96fa71c79581d9c3720b327706be5a184f55582f
Bill Mårtensson <bill@magictechnology.se>
2023-11-29 08:47:22 +0100

    Initial Commit

---
+++ Pia12nov29/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pia12nov29
+//
+//  Created by BillU on 2023-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pia12nov29/Pia12nov29App.swift
@@ -0,0 +1,17 @@
+//
+//  Pia12nov29App.swift
+//  Pia12nov29
+//
+//  Created by BillU on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct Pia12nov29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
